55c82a7a62d5cfabf2ee228b2ad15619bc7cb93e
myNodes12 <49424096+myNodes12@users.noreply.github.com>
2019-04-09 11:45:31 +0800

    Add files via upload

---
+++ INNOSPIREITSOLUTIONS.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+    <!-- FA Icons -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
+  integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    <!-- Icon -->
+    <link rel="shortcut icon" href="internal/img/logo.png">
+	<!--animation-->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css"href="internal/css_0/custom.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <!-- Bootstrap Javascript -->
+    <!-- jQuery, Popper.js, Bootstrap JS -->
+	    
+    <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" async></script>
+   -->
+   
+   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+   integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" async></script>
+   
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+   integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous" async></script>
+
+<script type="text/javascript"> $("html, body").animate({ scrollTop: 0 }, "slow");</script>
+
+    <title>Innospire IT Solutions</title>
+  </head>
+  
+  <!-- iconbar -->
+<link rel="stylesheet" type="text/css" href="internal/css_0/css_s.css">
+  <body>
+
+ <?php
+ include 'pages/navigation.php';
+ include 'pages/header_image.php';
+ include 'pages/about_us.php';
+ include 'pages/why_us.php';
+ include 'pages/services.php';
+ include 'pages/partners.php';
+ include 'pages/contact_us.php';
+ include 'pages/footer.php';
+ ?>
+	
+  </body>
+</html>
